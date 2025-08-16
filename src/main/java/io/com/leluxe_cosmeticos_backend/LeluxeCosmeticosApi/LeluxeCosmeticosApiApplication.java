47797601f39255f6a060f7d7da7041c9cd72814d
Dotyo Code <dotyomusic@gmail.com>
2025-08-16 15:13:54 -0300

    feature: boilerplate estrutura

--- src/main/java/io/com/leluxe_cosmeticos_backend/LeluxeCosmeticosApi/LeluxeCosmeticosApiApplication.java
+++ src/main/java/io/com/leluxe_cosmeticos_backend/LeluxeCosmeticosApi/LeluxeCosmeticosApiApplication.java
@@ -2,8 +2,10 @@ package io.com.leluxe_cosmeticos_backend.LeluxeCosmeticosApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LeluxeCosmeticosApiApplication {
 
 	public static void main(String[] args) {
