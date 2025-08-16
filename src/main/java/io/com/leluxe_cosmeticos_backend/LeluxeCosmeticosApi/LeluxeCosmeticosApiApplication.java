df104a83e15c201b56720043ee59aab974ae37be
Dotyo Code <dotyomusic@gmail.com>
2025-08-16 14:55:21 -0300

    feature: first commit

---
+++ src/main/java/io/com/leluxe_cosmeticos_backend/LeluxeCosmeticosApi/LeluxeCosmeticosApiApplication.java
@@ -0,0 +1,13 @@
+package io.com.leluxe_cosmeticos_backend.LeluxeCosmeticosApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeluxeCosmeticosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeluxeCosmeticosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/com/leluxe_cosmeticos_backend/LeluxeCosmeticosApi/LeluxeCosmeticosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.com.leluxe_cosmeticos_backend.LeluxeCosmeticosApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeluxeCosmeticosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
